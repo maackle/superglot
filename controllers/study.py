f0d3f1f34654ef695baa4eeccb818f596179de86
Michael Dougherty <maackle.d@gmail.com>
2014-09-22 23:49:40 -0700

    words get updated intervals; ability to determine if a word is "due"

--- controllers/study.py
+++ controllers/study.py
@@ -1,39 +1,40 @@
 import re
+import datetime
 from bs4 import BeautifulSoup
 import requests
 
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask.ext.login import current_user, login_required
 from flask.ext.babel import gettext as _
-from mongoengine.errors import NotUniqueError
 
 from cache import cache
-from forms import AddArticleForm
-import models
 from controllers import api
-from util import sorted_words
-import nlp
-import util
-import formatting
+import models
+
 
 
 blueprint = Blueprint('study', __name__, template_folder='templates')
 
 @blueprint.route('/')
+@login_required
 def home():
 	return render_template('views/study/home.jade')
 
 
 
 @blueprint.route('/words/')
+@login_required
 def words():
 
+	due_vocab = (item for item in current_user.vocab if item.next_repetition and item.next_repetition < datetime.datetime.now())
+
 	return render_template('views/study/study_words.jade', **{
-		
+		'due_vocab': due_vocab,
 	})
 
 
 @blueprint.route('/sentences/')
+@login_required
 def sentences():
 
 	sentences = set()
