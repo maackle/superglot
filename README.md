47c790b23bd7f5045740c670c521b6afe34a0333
Michael Dougherty <maackle.d@gmail.com>
2015-03-01 00:57:04 -0800

    new run file

--- README.md
+++ README.md
@@ -2,10 +2,25 @@
 Installation
 ============
 
-Babel
------
+### System requirements
+
+- python3.3+
+- python3-dev (3.3+ for newrelic)
+- libffi-dev (for bcrypt)
+
+### Babel
 
 Needs to be installed from github to get version 2.0dev. After pip install:
 
 cd ~/.envs/superglot/src/babel
 python setup.py import_cldr
+
+### NLTK
+
+Needs corpus (nltk_data).
+With GUI:
+
+```python
+import nltk
+nltk.download()
+```
--- run.py
+++ run.py
@@ -3,10 +3,7 @@ from logging.handlers import RotatingFileHandler
 
 from superglot import create_app
 
-
-if __name__ == '__main__':
-    app = create_app()
-    handler = RotatingFileHandler("log/error.log", maxBytes=10000000, backupCount=10)
-    handler.setLevel(logging.WARNING)
-    app.logger.addHandler(handler)
-    app.run(debug=True, port=app.config['SERVER_PORT'])
+app = create_app()
+handler = RotatingFileHandler("log/error.log", maxBytes=10000000, backupCount=10)
+handler.setLevel(logging.WARNING)
+app.logger.addHandler(handler)
