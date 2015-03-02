001888ed6730b2f987ca4f0a572461d8feae07e8
Michael Dougherty <maackle.d@gmail.com>
2015-03-02 07:28:43 -0800

    adds notes

--- superglot/config/envs/production.py
+++ superglot/config/envs/production.py
@@ -1,3 +1,8 @@
-DEBUG = True
+DEVELOPMENT = False
 
-SERVER_PORT = 8011
+SERVER_PORT = 31337
+BASE_URL = 'http://localhost:{}/'.format(SERVER_PORT)
+
+DATABASE_NAME = 'superglot_dev'
+SQLALCHEMY_DATABASE_URI = 'superglot_dev:superglot_dev@postgresql://localhost/{}'.format(DATABASE_NAME)
+ES_INDEX = 'superglot_dev'
