fecb6d94033642d95dee2913d547c5f40f1107b0
Superglot Staging <michael@superglot.com>
2015-03-02 23:11:45 -0500

    server stuff

--- bin/start-gunicorn.sh
+++ bin/start-gunicorn.sh
@@ -1,4 +1,4 @@
-
-NEW_RELIC_CONFIG_FILE=conf/newrelic.ini
-SUPERGLOT_SETTINGS=superglot/config/envs/production.py
-/home/superglot/.virtualenvs/superglot/bin/newrelic-admin run-program /home/superglot/.virtualenvs/superglot/bin/gunicorn -w 4 -b 127.0.0.1:6107 -t 240 --pythonpath /home/superglot/server superglot:app
+ROOT=/home/superglot/server
+export NEW_RELIC_CONFIG_FILE=$ROOT/conf/newrelic.ini
+export SUPERGLOT_SETTINGS="superglot.config.envs.staging"
+/home/superglot/.virtualenvs/superglot/bin/newrelic-admin run-program /home/superglot/.virtualenvs/superglot/bin/gunicorn -w 4 -b 127.0.0.1:6107 -t 240 --pythonpath $ROOT wsgi:app
