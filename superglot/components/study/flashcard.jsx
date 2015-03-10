9d3fe8347fe9426d08d582a525a36c0b75700bf7
Michael Dougherty <maackle.d@gmail.com>
2015-03-10 14:18:42 -0700

    more stuff on flashcard

--- superglot/components/study/flashcard.jsx
+++ superglot/components/study/flashcard.jsx
@@ -1,7 +1,18 @@
 
 
 var StudyPanel = React.createClass({
-    answerWord: function() {
+    getInitialState: function() {
+        return {
+            currentCard: 0,
+        };
+    },
+    answerCard: function(answer) {
+        if (answer > 0) { // a rating
+            // update rating and flip
+            this.setState({cardFlipped: true});
+        }
+    },
+    nextCard: function() {
 
     },
     render: function() {
@@ -9,8 +20,11 @@ var StudyPanel = React.createClass({
         return (
             <div className="study-panel">
                 <StudyTabs />
-                <Flashcard vword={ vocab[0] }
-                           answerWord={ this.answerWord }/>
+                { vocab.map( (v) => (
+                    <Flashcard vword={ v }
+                               answerCard={ this.answerCard }
+                    />
+                ))}
             </div>
         )
     }
@@ -29,55 +43,49 @@ var StudyTabs = React.createClass({
 });
 
 var Flashcard = React.createClass({
-    getInitialState: function() {
+    getInitialState: function () {
         return {
             isFlipped: false
         };
     },
+    answerCard: function(answer) {
+        this.setState({isFlipped: true});
+        return this.props.answerCard(answer);
+    },
     render: function() {
         var vword = this.props.vword;
         var contents = null;
-        var cardClass = this.props.isFlipped ? 'back' : 'front';
-        if (this.state.isFlipped) {
-            contents = (
-                <div>
-                    <h1>{ vword.word.lemma }</h1>
-                    <h2>You answered it.</h2>
-                </div>
-            );
-        } else {
-            contents = (
-                <div>
-                    <h1>{ vword.word.lemma }</h1>
-                    <AnswerSelector selectRating={ this.props.answerWord }/>
-                </div>
-            );
-        }
+        var cardClass = this.state.isFlipped ? 'flipped' : '';
         return (
-            <div className={ "card " + cardClass }>
-                { contents }
+            <div className={ "flashcard " + cardClass }>
+                <div className="flashcard-side flashcard-front">
+                    <h1 className="prompt">{ vword.word.lemma }</h1>
+                    <AnswerSelector selectRating={ this.props.answerCard } selectAction={ this.props.answerCard }/>
+                </div>
+                <div className="flashcard-side flashcard-back">
+                    <h1 className="prompt">{ vword.word.lemma }</h1>
+                    <h2 className="answer">(n.) cheeseball</h2>
+                    <a onClick={ this.props.nextCard }>next</a>
+                </div>
             </div>
         );
     }
 });
 
 var AnswerSelector = React.createClass({
-    selectRating: function(rating) {
-
-    },
     render: function() {
 
         return (
             <div className="flashcard-answer-selector">
                 <ul className="flashcard-answer-set flashcard-answer-ratings">
-                    <li data-rating={1} className="review-choice number" onClick={ this.props.selectRating(1) }>1</li>
-                    <li data-rating={2} className="review-choice number" onClick={ this.props.selectRating(2) }>2</li>
-                    <li data-rating={3} className="review-choice number" onClick={ this.props.selectRating(3) }>3</li>
+                    <li data-rating={1} className="review-choice number" onClick={ () => this.props.selectRating(1) }>1</li>
+                    <li data-rating={2} className="review-choice number" onClick={ () => this.props.selectRating(2) }>2</li>
+                    <li data-rating={3} className="review-choice number" onClick={ () => this.props.selectRating(3) }>3</li>
                 </ul>
                 <ul className="flashcard-answer-set flashcard-answer-other">
-                    <li className="review-choice other">⃠</li>
-                    <li className="review-choice other">&times;</li>
-                    <li className="review-choice other">(bury)</li>
+                    <li className="review-choice other" onClick={ () => this.props.selectAction(0) }>&times;</li>
+                    <li className="review-choice other" onClick={ () => this.props.selectAction(-1) }>⃠</li>
+                    <li className="review-choice other" onClick={ () => this.props.selectAction('bury') }>(bury)</li>
                 </ul>
             </div>
         );
