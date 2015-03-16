2410f8c9c1ac5cdd07075fb57d0029cc933baf2f
Michael Dougherty <maackle.d@gmail.com>
2015-03-15 22:24:20 -0700

    study route rework

--- superglot/blueprints/frontend/__init__.py
+++ superglot/blueprints/frontend/__init__.py
@@ -1,12 +1,18 @@
 from flask import Blueprint, render_template
 from flask.ext.login import current_user
 
+from superglot import core
+
 blueprint = Blueprint('frontend', __name__, template_folder='templates')
 
 
 @blueprint.route('/')
 def home():
     if current_user.is_authenticated():
-        return render_template('views/user/dashboard.jade')
+        due_vocab = core.gen_due_vocab(current_user)
+        ctx = {
+            'due_vocab_count': len(due_vocab)
+        }
+        return render_template('views/user/dashboard.jade', **ctx)
     else:
         return render_template('views/home.jade')
